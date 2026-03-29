9a94a189b5b85391066b5082642a49f21b69f57c
Sofia SH <shulzhenko.sofiia1124@vu.cdu.edu.ua>
2026-03-29 18:48:00 +0300

    Add some logic into MainWindow.xaml

--- calculator/calculator/CalculatorCommands.cs
+++ calculator/calculator/CalculatorCommands.cs
@@ -1,12 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media.Animation;
+using System.Windows.Media;
 
-namespace calculator
+namespace lab2_wpf
 {
-    class CalculatorCommands
+    public partial class MainWindow : Window
     {
+        Stack<CalculatorCommand> undoStack = new Stack<CalculatorCommand>();
+        Stack<CalculatorCommand> redoStack = new Stack<CalculatorCommand>();
+
+        public bool IsNewEntry { get; set; } = true;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Execute(CalculatorCommand command)
+        {
+            command.Execute();
+            if (command.HasStateChanged())
+            {
+                undoStack.Push(command);
+                redoStack.Clear();
+            }
+        }
+
+       
     }
 }
